acde58d2c2fa46fb01f4debb97e238df909d5470
kanishka2345 <150791892+kanishka2345@users.noreply.github.com>
2024-04-23 09:25:24 +0530

    Add files via upload

---
+++ day6/webelement/src/test/java/com/classwork/webelement/WebelementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.classwork.webelement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebelementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
