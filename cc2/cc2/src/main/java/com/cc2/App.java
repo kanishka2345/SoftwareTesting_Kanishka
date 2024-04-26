9dc0f7be5f0a1ebfd30cdfcefbef61ac7933d622
kanishka2345 <150791892+kanishka2345@users.noreply.github.com>
2024-04-26 20:39:19 +0530

    Add files via upload

---
+++ cc2/cc2/src/main/java/com/cc2/App.java
@@ -0,0 +1,28 @@
+package com.cc2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.idealized.Javascript;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        // WebDriver driver=new ChromeDriver();
+        // driver.get("https://www.barnesandnoble.com/");
+        // driver.findElement(By.xpath("//*[@id=\"rhf_header_element\"]/nav/div/div[3]/form/div/div[1]/a")).click();
+        // driver.findElement(By.xpath("//*[@id='rhf_header_element']/nav/div/div[3]/form/div/div[1]/div/a[2]")).click();
+        // driver.findElement(By.xpath("//*[@id='rhf_header_element']/nav/div/div[3]/form/div/div[2]/div/input[1]")).sendKeys("Chetan Bhagat");
+        // driver.findElement(By.xpath("//*[@id='rhf_header_element']/nav/div/div[3]/form/div/span")).click();
+      
+
+        
+        System.out.println( "Hello World!" );
+        
+    }
+}
